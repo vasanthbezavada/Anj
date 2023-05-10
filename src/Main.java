89580dc2bddfa8a806453a6d507fa5dcfef0268c
vasanth <kumarbezavada@gmail.com>
2023-05-10 23:02:47 +0530

    first

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        int a ;
+        a = 10;
+        String abe = "0";
+        System.out.println(a);
+    }
+}
