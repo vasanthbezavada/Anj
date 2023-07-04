48a2ddee0a754139fe895b3d67584652ebbd02f5
vasanth <kumarbezavada@gmail.com>
2023-07-04 19:07:41 +0530

    added streams

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-        int a ;
-        a = 10;
-        String abe = "0";
-        System.out.println(a);
+        List<Integer> integers = Arrays.asList(1,2,3,5,4,6,7,2,3,2,1,3,7,8,9,2,3,7,1);
+        Collections.sort(integers);
     }
 }
